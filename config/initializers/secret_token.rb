9f6f03775a1e7ed777c964eb3fdfca4a447abc86
Seth Holloway <smholloway@gmail.com>
2011-02-11 15:07:19 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StopRepeatingYourself::Application.config.secret_token = '3fb376e0bf24a314e4b1a2d167b628d70e19d0346e88bf1c28e8bbca6ff6e9211bfff15f6410c736ab6826acb66f8f2dbc7b90c11182be6ff1f47b6fdb4d4606'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+StopRepeatingYourself::Application.routes.draw do
+  get "home/index"
+  root :to => "home#index"
+end
