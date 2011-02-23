c677b00deaa5dcbaa7dcc93105a2d1c8849fde7d
Seth Holloway <smholloway@gmail.com>
2011-02-23 14:52:03 -0600

    added stuff from Google Doc to live page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 StopRepeatingYourself::Application.routes.draw do
-  get "home/index"
-  root :to => "home#index"
+  match 'home' => 'home#index', :via => :get
+  match 'about' => 'home#about', :via => :get
+  match 'contact' => 'home#contact', :via => :get
+
+  root :to => "home#index" # define the root path
+
+  match '*a' => redirect('/') # send all random routes to home
 end
