7f3ef7b04b6aaf8e101ac65b88988ac75fe4d84e
Seth Holloway <smholloway@gmail.com>
2011-03-02 20:07:14 -0600

    added sections for the problem, our approach, and how it works. updated wording from google doc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 StopRepeatingYourself::Application.routes.draw do
+
   match 'home' => 'home#index', :via => :get
   match 'about' => 'home#about', :via => :get
   match 'contact' => 'home#contact', :via => :get
+  match 'problem' => 'home#problem', :via => :get
+  match 'module' => 'home#module', :via => :get
+  match 'technology' => 'home#technology', :via => :get
 
   root :to => "home#index" # define the root path
 
