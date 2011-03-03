fad5c11d7a212d72bed9d75d93bed2823a536023
Seth Holloway <smholloway@gmail.com>
2011-03-02 20:39:40 -0600

    added caching and slight style tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,26 @@
 class HomeController < ApplicationController
+  def about
+    response.headers['Cache-Control'] = 'public, max-age=1314000'
+  end
+
+  def contact
+    response.headers['Cache-Control'] = 'public, max-age=1314000'
+  end
+
   def index
+    response.headers['Cache-Control'] = 'public, max-age=1314000'
+  end
+
+  def module
+    response.headers['Cache-Control'] = 'public, max-age=1314000'
+  end
+
+  def problem
+    response.headers['Cache-Control'] = 'public, max-age=1314000'
+  end
+
+  def technology
+    response.headers['Cache-Control'] = 'public, max-age=1314000'
   end
 
 end
