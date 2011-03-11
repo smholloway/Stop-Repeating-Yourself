499467c6f59d2f7f6fbab85bd1488a53a9623a4a
Seth Holloway <smholloway@gmail.com>
2011-03-11 14:20:24 -0600

    added notice and alert class to css, fixed titles in existing pages, added email option in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,16 @@ StopRepeatingYourself::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'projectspeakeasy.mailer@gmail.com',
+    :password             => 'astrongpassword!',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
