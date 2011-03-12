13c7cecb86ab7f7e82ad84997797a46ce592bf69
Seth Holloway <smholloway@gmail.com>
2011-03-12 10:08:39 -0600

    added gravatars for future use with user profiles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     content_for(:title) { " - " + page_title }
   end
 
+  def gravatar_for email, options = {}
+    options = {:alt => 'avatar', :class => 'avatar', :size => 80}.merge! options
+    id = Digest::MD5::hexdigest email.strip.downcase
+    url = 'http://www.gravatar.com/avatar/' + id + '.jpg?s=' + options[:size].to_s
+    options.delete :size
+    image_tag url, options
+  end
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 StopRepeatingYourself::Application.initialize!
+
+# Including digest so we can grab Gravatar images using the email hash
+require 'digest'
